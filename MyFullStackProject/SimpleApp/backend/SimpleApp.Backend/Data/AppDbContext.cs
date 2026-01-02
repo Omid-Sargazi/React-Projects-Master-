d7100dc202d5a245c695f20127193cadff375127
Omid-Sargazi <om.sargazi@gmail.com>
2026-01-02 11:51:21 +0330

    add Tasks DbSet to AppDbContext

--- MyFullStackProject/SimpleApp/backend/SimpleApp.Backend/Data/AppDbContext.cs
+++ MyFullStackProject/SimpleApp/backend/SimpleApp.Backend/Data/AppDbContext.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SimpleApp.Backend.Models;
 
 namespace SimpleApp.Backend.Data
 {
     public class AppDbContext:IdentityDbContext
     {
          public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-             {
-        }
+            : base(options){}
+            public DbSet<TaskItem> Tasks => Set<TaskItem>();
     }
 }
