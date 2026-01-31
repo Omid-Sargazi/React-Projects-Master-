e513fe23a40cd9ba5ea3b5fe29bf59b6b8288dbe
Omid-Sargazi <om.sargazi@gmail.com>
2026-01-31 10:44:54 +0330

    add todo list and item components

--- SimnpleReact/frontend/src/Components/TodoItem.jsx
+++ SimnpleReact/frontend/src/Components/TodoItem.jsx
@@ -1,7 +1,20 @@
-export default function TodoItem()
+export default function TodoItem({ todo, onToggle, onRemove })
 {
     return(
         <>
+
+        <li>
+      <span
+        style={{
+          textDecoration: todo.completed ? "line-through" : "none",
+          cursor: "pointer"
+        }}
+        onClick={() => onToggle(todo.id)}
+      >
+        {todo.title}
+      </span>
+      <button onClick={() => onRemove(todo.id)}>❌</button>
+    </li>
         </>
     )
 }
--- SimnpleReact/frontend/src/Components/TodoList.jsx
+++ SimnpleReact/frontend/src/Components/TodoList.jsx
@@ -1,7 +1,19 @@
+import TodoItem from "./TodoItem";
+
 export default function TodoList()
 {
     return(
         <>
+        <ul>
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onToggle={onToggle}
+          onRemove={onRemove}
+        />
+      ))}
+    </ul>
         </>
     )
 }
