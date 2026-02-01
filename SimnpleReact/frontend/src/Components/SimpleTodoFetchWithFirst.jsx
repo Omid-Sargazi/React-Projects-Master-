033138205b39ca3a6b5924678aa195b7a73c751d
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-01 07:54:45 +0330

    fetch by useEffect and seperated tasks todos

--- SimnpleReact/frontend/src/Components/SimpleTodoFetchWithFirst.jsx
+++ SimnpleReact/frontend/src/Components/SimpleTodoFetchWithFirst.jsx
@@ -1,7 +1,38 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 export default function SimpleTodoFetchWithFirst()
 {
     const [filter, setFilter] = useState("all");
+
+    const [todos, setTodos] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+
+    useEffect(()=>{
+        setLoading(true);
+
+        fetch("https://jsonplaceholder.typicode.com/todos")
+        .then(res=>res.json())
+        .then(data=>{
+            let filtered = data;
+
+            if(filtered==="copleted")
+            {
+                filtered = data.filter(t=>t.completed);
+            }
+
+            if (filter === "not_completed") {
+            filtered = data.filter(t => !t.completed);
+            }
+
+            setTodos(filtered.slice(0,10));
+            setLoading(false);
+        }).catch(()=>{
+            setError("Failed to load todos")
+            .setLoading(false);
+        })
+    },[filter])
+
     return(
         <>
         <div>
@@ -13,8 +44,15 @@ export default function SimpleTodoFetchWithFirst()
             <option value="not_completed">Not Completed</option>
         </select>
 
+       {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+
         <ul>
-            <li>Todo example</li>
+        {todos.map(todo => (
+            <li key={todo.id}>
+            {todo.title} {todo.completed ? "✅" : "❌"}
+            </li>
+        ))}
         </ul>
         </div>
         </>
