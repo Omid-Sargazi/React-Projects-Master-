7753c0d4a318af4e7be1ccd7984761e33db6e781
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-02 14:58:41 +0330

    Create  reducer

---
+++ SimnpleReact/frontend/src/Components/counterReducer.jsx
@@ -0,0 +1,14 @@
+import { useReducer } from "react";
+
+function counterReducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      return { count: 0 };
+    default:
+      return state;
+  }
+}
