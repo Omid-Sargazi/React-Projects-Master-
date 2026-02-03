900905601136bccfebf787fe466b6874b32e9c16
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-03 14:07:33 +0330

    implement basic server-side pagination using query params

--- next-project/app/error.tsx
+++ next-project/app/error.tsx
@@ -7,3 +7,51 @@ export default function Error({reset}:{reset:()=>void})
         </div>
     )
 }
+
+function Pagination({
+    page,
+    total,
+    onNext,
+    onPrev,
+}:{
+    page:number,
+    total:number,
+    onNext:()=>void;
+    onPrev:()=>void;
+})
+{
+    return (
+        <div>
+            <span>{page}/{total}</span>
+            <button onClick={onPrev}>Prev</button>
+            <button onClick={onNext}>Next</button>
+        </div>
+    )
+}
+
+function SearchBox({
+    value,
+    onChange,
+}:{
+    value:string;
+    onChange:(value:string)=>void;
+})
+{
+    return(
+        <div>
+            <input value={value} 
+            onChange={e=>onChange(e.target.value)}
+            />
+        </div>
+    )
+}
+
+
+function Page2({
+    params,
+}:{
+    params:{id:string}
+})
+{
+    return <h1>Iser {params.id}</h1>
+}
--- next-project/app/page.tsx
+++ next-project/app/page.tsx
@@ -9,22 +9,44 @@ import UserSearch from "./UserSearch";
 //   );
 // }
 
-
-async function getUsers()
+type User =
 {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users",{cache:"no-store"});
+  id:number;
+  name:string;
+}
+
+
+async function getUsers(page:number):Promise<User[]>
+{
+  const limit = 5;
+  const start = (page-1) * limit;
+
+
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users?_start=${start}&_limit=${limit}`,{cache:"no-store"});
 
   return res.json();
   
 }
 
-export default async function Page()
+export default async function Page({
+  searchParams,
+}:{searchParams:{page?:string};})
 {
-  const users = await getUsers();
+  const page = Number(searchParams.page ?? 1);
+  const users = await getUsers(page);
 
   return(
     <div>
       <h1>User Dashboard</h1>
+      <h1>Users(Page {page})</h1>
+
+      <ul>
+        {users.map(user=>(
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <a href={`/?page=${page-1}`}>Prev</a>{" "}
+      <a href={`/?page=${page+1}`}>Next</a>
 
       {/* <ul>
         {users.map((user:any)=>(
