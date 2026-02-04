6c56aa8b4fe8addba8c1d5700904a10952357aa8
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-04 09:18:45 +0330

    handle login flow using server actions and auth context

--- next-project/app/page.tsx
+++ next-project/app/page.tsx
@@ -28,33 +28,67 @@ async function getUsers(page:number):Promise<User[]>
   
 }
 
-export default async function Page({
-  searchParams,
-}:{searchParams:{page?:string};})
-{
-  const page = Number(searchParams.page ?? 1);
-  const users = await getUsers(page);
+// export default async function Page({
+//   searchParams,
+// }:{searchParams:{page?:string};})
+// {
+//   const page = Number(searchParams.page ?? 1);
+//   const users = await getUsers(page);
 
-  return(
+//   return(
+//     <div>
+//       <h1>User Dashboard</h1>
+//       <h1>Users(Page {page})</h1>
+
+//       <ul>
+//         {users.map(user=>(
+//           <li key={user.id}>{user.name}</li>
+//         ))}
+//       </ul>
+//       <a href={`/?page=${page-1}`}>Prev</a>{" "}
+//       <a href={`/?page=${page+1}`}>Next</a>
+
+//       {/* <ul>
+//         {users.map((user:any)=>(
+//           <li key={user.id}>{user.name}</li>
+//         ))}
+//       </ul> */}
+
+//       <UserSearch users={users}/>
+//     </div>
+//   )
+// }
+
+
+
+"use client";
+
+import { loginAction } from "./loginAction";
+import { useAuth } from "./AuthProvider";
+
+export default function Page() {
+  const { user, login, logout } = useAuth();
+
+  if (!user) {
+    return (
+      <form
+        action={async (formData) => {
+          const user = await loginAction(formData);
+          login(user);
+        }}
+      >
+        <h1>Login</h1>
+        <input name="username" placeholder="Username" />
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+
+  return (
     <div>
-      <h1>User Dashboard</h1>
-      <h1>Users(Page {page})</h1>
-
-      <ul>
-        {users.map(user=>(
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <a href={`/?page=${page-1}`}>Prev</a>{" "}
-      <a href={`/?page=${page+1}`}>Next</a>
-
-      {/* <ul>
-        {users.map((user:any)=>(
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul> */}
-
-      <UserSearch users={users}/>
+      <h1>Dashboard</h1>
+      <p>Welcome {user.name}</p>
+      <button onClick={logout}>Logout</button>
     </div>
-  )
+  );
 }
