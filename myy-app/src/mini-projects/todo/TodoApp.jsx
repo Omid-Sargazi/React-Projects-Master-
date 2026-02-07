62d08d1f27ed5033cc8371a0a9ad1679cfb10d8e
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-07 13:44:17 +0330

    .

--- myy-app/src/mini-projects/todo/TodoApp.jsx
+++ myy-app/src/mini-projects/todo/TodoApp.jsx
@@ -73,23 +73,28 @@ export default function TodoApp()
         }
 
 
-    return(
-        <>
-        <div className="todo-app">
-             <h2>Todo Mini Project</h2>
+     return (
+    <div className="todo-app">
+      <h2>Todo Mini Project</h2>
 
-        <div>
-        <input type="text" placeholder="Add new task" />
-        <button>Add</button>
-        </div>
-        <ul>
-        <li>
-          <input type="checkbox" />
-          Learn React
-          <button>Delete</button>
-        </li>
-      </ul>
-        </div>
-        </>
-    )
+      <div>
+        <input
+          value={newTodo}
+          onChange={e => setNewTodo(e.target.value)}
+          placeholder="Add new task"
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+
+      {loading ? (
+        <p>Loading todos...</p>
+      ) : (
+        <TodoList
+          todos={todos}
+          onToggle={toggleTodo}
+          onDelete={deleteTodo}
+        />
+      )}
+    </div>
+  );
 }
