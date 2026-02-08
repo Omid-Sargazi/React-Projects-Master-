fd7c06a26d44918342eea51b088b4f74b07d684d
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-08 07:37:47 +0330

    add users mini project with search and memoized filtering

--- myy-app/src/mini-projects/users/UsersApp.jsx
+++ myy-app/src/mini-projects/users/UsersApp.jsx
@@ -1,17 +1,55 @@
+import { useEffect, useMemo, useState } from "react";
+import UserCard from "./UserCard";
+
 export default function UsersApp() {
+
+    const [users, setUsers] = useState([]);
+    const [search, setSearch] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        fetch("https://randomuser.me/api/?results=10")
+        .then(res=>res.json())
+        .then(data=>{
+            setUsers(data.results);
+            setLoading(false);
+        })
+    },[]);
+
+    const filteredUsers = useMemo(() => {
+    return users.filter(user =>
+      `${user.name.first} ${user.name.last}`
+        .toLowerCase()
+        .includes(search.toLowerCase())
+    );
+  }, [users, search]);
+
+  
+
+
+
+
   return (
     <div>
       <h2>Users Mini Project</h2>
 
-      <input placeholder="Search user..." />
+      <input
+        placeholder="Search user..."
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
 
-      <div>
-        <div className="user-card">
-          <img src="" alt="user" />
-          <h3>John Doe</h3>
-          <p>Email: john@example.com</p>
+      {loading ? (
+        <p>Loading users...</p>
+      ) : (
+        <div>
+          {filteredUsers.map(user => (
+            <UserCard key={user.login.uuid} user={user}>
+              <p>Email: {user.email}</p>
+            </UserCard>
+          ))}
         </div>
-      </div>
+      )}
     </div>
   );
 }
