b3a8ae8acf65e0c757cbd24dc20008322e3ef93e
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-08 11:50:13 +0330

    build smart counter step by step with props and useCallback

--- myy-app/src/mini-projects/Counter/CounterApp.jsx
+++ myy-app/src/mini-projects/Counter/CounterApp.jsx
@@ -1,24 +1,32 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export default function CounterApp()
 {
     const [count, setCount] = useState(0)
 
-    function increase()
-    {
-        setCount(count +1)
-        // console.log("render")
-    }
-    function decrease()
-    {
-        setCount(prevCount=>{
-            if(prevCount<=0)
-            {
-                return 0;
-            }
-            return prevCount -1;
-        })
-    }
+    // function increase()
+    // {
+    //     setCount(count +1)
+    //     // console.log("render")
+    // }
+    // function decrease()
+    // {
+    //     setCount(prevCount=>{
+    //         if(prevCount<=0)
+    //         {
+    //             return 0;
+    //         }
+    //         return prevCount -1;
+    //     })
+    // }
+
+    const increase = useCallback(()=>{
+        setCount(c=>c+1)
+    },[count]);
+
+    const decrease = useCallback(()=>{
+        setCount(c=>c-1)
+    },[count])
         console.log("render")
 
     return(
