4d53ee554c05ce377ba984f5866e37395b0fe940
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-14 08:14:19 +0330

    add ref and focus on project

--- myy-app/src/mini-projects/TodoList/TodoList.jsx
+++ myy-app/src/mini-projects/TodoList/TodoList.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 
 export default function TodoList()
 {
     const [task, setTask] = useState("")
     const [todos, setTodos] = useState([]);
+    const inputRef = useRef(null);
+    const deleteRef = useRef(null);
+
+    useEffect(()=>{
+        inputRef.current?.focus();
+
+    },[])
 
     const addTask = ()=>{
         if(!task.trim()) return;
 
         setTodos([...todos,{id:Date.now(),text:task}]);
         setTask("");
+        inputRef.current?.focus();
     }
 
 
@@ -21,14 +29,25 @@ export default function TodoList()
     <div class="container">
         <h1>Todo List</h1>
 
-        <input value={task}  onChange={(e)=>setTask(e.target.value)}  type="text" placeholder="New task..." />
-        <button onClick={addTask}>Add</button>
+        <input ref={inputRef} value={task}  onChange={(e)=>setTask(e.target.value)}  type="text" placeholder="New task..." />
+        <button onKeyDown={e=>{
+            if(e.key=="Enter")
+            {
+                inputRef.current.focus();
+            }
+        }}  onClick={addTask}>Add</button>
 
        <ul>
         {todos.map(todo => (
           <li key={todo.id}>
             {todo.text}
-            <button onClick={() => deleteTask(todo.id)}>
+            <button onKeyDown={e=>{
+                if(e.key==="Enter")
+                {
+                    // e.preventDefault();
+                    deleteRef.current.focus();
+                }
+            }} ref={deleteRef} onClick={() => deleteTask(todo.id)}>
               Delete
             </button>
           </li>
