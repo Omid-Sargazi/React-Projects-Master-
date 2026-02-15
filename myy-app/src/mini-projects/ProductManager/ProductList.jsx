eb889b100737abf217c3245148b3cbde2b43a1be
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-15 08:39:02 +0330

    add ProductList component with conditional rendering and mapping

--- myy-app/src/mini-projects/ProductManager/ProductList.jsx
+++ myy-app/src/mini-projects/ProductManager/ProductList.jsx
@@ -1,7 +1,22 @@
-export default function ProductList()
+import ProductItem from "./ProductItem"
+
+export default function ProductList({items, onDelete})
 {
+    if(items.length====0)
+    {
+        return <p>No product has been added yet.</p>
+    }
     return(
         <>
+        <ul>
+            {items.map(item=>(
+                <ProductItem
+                    key={item.id}
+                    product={item}
+                    onDelete={onDelete}
+                />
+            ))}
+        </ul>
         </>
     )
 }
